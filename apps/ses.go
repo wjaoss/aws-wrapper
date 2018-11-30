573d28ffdc82c8f3971b36dee0f583bcd04bb669
bayupermadi <irwanhikmah@gmail.com>
2018-11-30 16:32:53 +0700

    add ses wrapper

---
+++ apps/ses.go
@@ -0,0 +1,72 @@
+package apps
+
+import (
+	"fmt"
+
+	//go get -u github.com/aws/aws-sdk-go
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/wjaoss/aws-wrapper/session"
+)
+
+const (
+	// The character encoding for the email.
+	CharSet = "UTF-8"
+)
+
+func SES(from string, to string, subject string, body string) {
+
+	sess := session.StartNewSession()
+
+	// Create an SES session.
+	svc := ses.New(sess)
+
+	// Assemble the email.
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(to),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(body),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(CharSet),
+				Data:    aws.String(subject),
+			},
+		},
+		Source: aws.String(from),
+	}
+
+	// Attempt to send the email.
+	result, err := svc.SendEmail(input)
+
+	// Display error messages if they occur.
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			fmt.Println(err.Error())
+		}
+
+		return
+	}
+
+	fmt.Println(result)
+}
--- sample.go
+++ sample.go
@@ -1,17 +1,19 @@
 package main
 
 import (
+	"github.com/wjaoss/aws-wrapper/apps"
 	"github.com/wjaoss/aws-wrapper/session"
 	"github.com/wjaoss/aws-wrapper/tools"
 )
 
-func main() {
+func this() {
 
-	awsKeyID := "your ID"
-	awsSecretKey := "your Key Secret very"
-	awsRegion := "us-east-1"
+	awsKeyID := "your-ID"
+	awsSecretKey := "your-secret-key"
+	awsRegion := "region"
 
 	session.SetConfiguration(awsKeyID, awsSecretKey, awsRegion)
 	tools.CW("Testing", "Logsize", "Megabytes", 10.3, "LogFile", "/tmp/me.txt")
+	apps.SES("sender@gmail.com", "recipient@gmail.com", "subject", "body")
 
 }
